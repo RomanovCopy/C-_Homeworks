fc2c8bfa6d3e52a7e89b07f9e4be8ac1c6cce209
Romanov Sergey <romanovcopy@gmail.com>
2023-01-28 17:50:51 +0300

    Task 2 solved

--- Task_2/Program.cs
+++ Task_2/Program.cs
@@ -1,2 +1,7 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Я скажу какое число больше...\n");
+Console.WriteLine("Введите первое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+int c = a > b ? a : b;
+Console.WriteLine($"Из двух чисел {a} и {b} большим является {c}");
