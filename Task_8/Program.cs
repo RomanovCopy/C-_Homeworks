469fecf2cbd8a7a3003c977096d4ac990e4737ac
Romanov Sergey <romanovcopy@gmail.com>
2023-01-28 18:59:44 +0300

    Task 8 solved

--- Task_8/Program.cs
+++ Task_8/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+Console.Write("Все четные числа из интервала от 1 до введеного\nВведите число ");
+int n = Convert.ToInt32(Console.ReadLine());
+string result = "";
+int c = n > 1 ? -1 : 1;
+for (int a = n; a != 0; a = a + c)
+{
+    result = a % 2 == 0 ? $"{result} {a}" : result;
+}
+Console.WriteLine(result.Length > 1 ? $"Все четные числа из интервала от 1 до {n}: " : $"В интервале от 1 до {n} четных чисел нет.");
+Console.Write(result);
