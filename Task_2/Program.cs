b5cd0f9aae73c783247b3b6e62f9a0bcd6120d1e
Romanov Sergey <romanovcopy@gmail.com>
2023-01-29 15:03:59 +0300

    end

--- Task_2/Program.cs
+++
@@ -1,7 +0,0 @@
-﻿Console.Clear();
-Console.Write("Какое число больше...?\nВведите первое число: ");
-int a = Convert.ToInt32(Console.ReadLine());
-Console.Write("Введите второе число: ");
-int b = Convert.ToInt32(Console.ReadLine());
-int c = a > b ? a : b;
-Console.WriteLine($"Из двух чисел {a} и {b} большим является {c}");
--- Task_6/Program.cs
+++
@@ -1,5 +0,0 @@
-﻿Console.Clear();
-Console.Write("Является ли введеное число четным...?\nВведите число: ");
-int a = Convert.ToInt32(Console.ReadLine());
-bool result = a % 2 == 0;
-Console.WriteLine(result ? $"Число {a} является четным." : $"Число {a} не является четным.");
