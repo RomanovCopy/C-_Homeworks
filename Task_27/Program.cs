47b65b7f42bbb5badedea0abbf320897f9d733dc
Romanov Sergey <romanovcopy@gmail.com>
2023-02-05 17:29:37 +0300

    Task_27 Ok

--- Task_27/Program.cs
+++ Task_27/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*Напишите программу, которая принимает на вход
+ число и выдаёт сумму цифр в числе.*/
+Console.Clear();
+Console.Write("Введите число:");
+int n = Convert.ToInt32(Console.ReadLine());
+int n1 = n < 0 ? n * -1 : n;
+int s = 0;
+while (n1 > 0)
+{
+    s = s + n1 % 10;
+    n1 = n1 / 10;
+}
+Console.Write($"Сумма цифр в числе {n}: {s} ");
