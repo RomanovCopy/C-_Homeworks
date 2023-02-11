63cf5a77c41fb042ab979799f3c6ef5dca1b2e76
Romanov Sergey <romanovcopy@gmail.com>
2023-02-11 12:10:30 +0300

    Task 36 End

--- Task_36/Program.cs
+++ Task_36/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*Задайте одномерный массив, заполненный случайными числами. 
+ * Найдите сумму элементов, стоящих на нечётных позициях.*/
+double[] CreateArray(int n)
+{
+    double[] array = new double[n];
+    for (int i = 0; i < array.Length; i++)
+        array[i] = new Random().Next(-100, 100);
+    return array;
+}
+
+double SumOfOddItems(double[] array)
+{
+    double sum = 0;
+    for (int i = 1; i < array.Length; i += 2)
+        sum += array[i];
+    return sum;
+}
+double[] array = CreateArray(20);
+Console.WriteLine($"Получен массив : {string.Join(" ; ", array)}");
+Console.Write($"Сумма элементов на нечетных позициях в массиве : {SumOfOddItems(array)}");
+Console.ReadKey();
+
+
