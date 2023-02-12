d0a178ae89ec8b70c1f44fbef36d142ef7e51a73
Romanov Sergey <romanovcopy@gmail.com>
2023-02-12 18:36:31 +0300

    Task41 End

--- Task41/Program.cs
+++ Task41/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.*/
+Console.Write("Сколько чисел будет введено? : ");
+int n = Convert.ToInt32(Console.ReadLine());
+double i=0;
+string m = string.Empty;
+int count = 0;
+do
+{
+    Console.Write($"Осталось : {n}\n Ваше число : ");
+    i = Convert.ToDouble(Console.ReadLine());
+    Console.Clear();
+    count += i > 0 ? 1 : 0;
+    m = $"{m} {i}";
+    n--;
+    Console.WriteLine(m);
+}
+while (n > 0);
+Console.Write($"Введено {count} положительный чисел.");
+Console.ReadKey();
