6f3621190fca8c892751e9384ec145162bc80642
Romanov Sergey <romanovcopy@gmail.com>
2023-02-18 18:04:49 +0300

    additional task2 end

--- Additional_Task2/Program.cs
+++ Additional_Task2/Program.cs
@@ -1,2 +1,49 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write( "Размер изображения : " );
+int[] dim = Console.ReadLine().Split().Select( ( x ) => int.Parse( x ) ).ToArray();
+char[,] array = CreateArray( dim[0], dim[1] );
+Console.WriteLine( "Первичный массив : " );
+PrintArray( array );
+char[,] array2 = CreateArray( dim[0], dim[1] );
+Console.WriteLine( "Вторичный массив : " );
+PrintArray( array );
+Console.Write( $"Не совпадает {contains( array, array2 )} пикселей." );
+Console.ReadKey();
+
+
+
+
+int contains( char[,]a, char[,] b )
+{
+    int count = 0;
+    for (int i = 0; i < a.GetLength( 0 ); i++)
+    {
+        for (int j = 0; j < a.GetLength( 1 ); j++)
+        {
+            count += a[i, j] == b[i, j] ? 1 : 0;
+        }
+    }
+    return count;
+}
+char[,] CreateArray( int lines, int columns )
+{
+    char[,] array = new char[lines, columns];
+    for (int i = 0; i < array.GetLength( 0 ); i++)
+    {
+        for (int j = 0; j < array.GetLength( 1 ); j++)
+        {
+            array[i, j] = new Random().Next( 0, 2 ) == 1 ? 'B' : 'W';
+        }
+    }
+    return array;
+}
+void PrintArray( char[,] array )
+{
+    for (int i = 0; i < array.GetLength( 0 ); i++)
+    {
+        for (int j = 0; j < array.GetLength( 1 ); j++)
+        {
+            Console.Write( $"{array[i, j]}\t" );
+        }
+        Console.WriteLine();
+    }
+}
