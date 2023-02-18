3657520218bf63b2392372a88709432f2f69343d
Romanov Sergey <romanovcopy@gmail.com>
2023-02-18 14:43:09 +0300

    end

--- AdditionalTask1/Program.cs
+++ AdditionalTask1/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*Дана строка, состоящая из N попарно различных символов. 
+ * Требуется вывести все перестановки символов данной строки.*/
+Console.Write("Введите строку : ");
+char[] str = Console.ReadLine().ToArray();
+for(int i=0; i < str.Length-1; i++)
+{
+    for (int j = i+1; j < str.Length; j++)
+    {
+        if (i != j)
+            Console.WriteLine(string.Join(" ", search(i, j)));
+    }
+}
+Console.ReadKey();
+
+char[] search(int ch1, int ch2)
+{
+    char[] res = new char[str.Length];
+    str.CopyTo(res, 0);
+    char temp = res[ch1];
+    res[ch1] = res[ch2];
+    res[ch2] = temp;
+    return res; 
+}
