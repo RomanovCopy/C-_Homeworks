031de6b9590f43d74e45a969ed54dcc711c32369
Romanov Sergey <romanovcopy@gmail.com>
2023-02-18 22:18:08 +0300

    Additionaltask3 end

--- Additional_task3/Program.cs
+++ Additional_task3/Program.cs
@@ -1,2 +1,50 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+Console.Write( "Размерность массива : " );
+int[] dim = Console.ReadLine().Split().Select( ( x ) => int.Parse( x ) ).ToArray();
+int[,] array = CreateArray( dim[0], dim[1] );
+PrintArray( array );
+Console.ReadKey();
+
+
+
+
+
+int[,] CreateArray( int lines, int columns )
+{
+    int value = 0;
+    int dif = 2;
+    int[,] array = new int[lines, columns];
+    for (int i = 0; i < array.GetLength( 0 ); i++)
+    { //начальные точки всех строк
+        array[i, 0] = value;
+        value += dif;
+        dif++;
+    }
+    dif = 1;
+    //заполняем строки до конца
+    for (int i = 0; i < lines; i++)
+    {
+        dif = i + 1;
+        value = array[i, 0];
+        for (int j = 1; j < columns; j++)
+        {
+            value += dif;
+            array[i, j] = value;
+            dif++;
+        }
+    }
+    return array;
+}
+
+
+void PrintArray( int[,] array )
+{
+    for (int i = 0; i < array.GetLength( 0 ); i++)
+    {
+        for (int j = 0; j < array.GetLength( 1 ); j++)
+        {
+            Console.Write( $"{array[i, j]}\t" );
+        }
+        Console.WriteLine();
+    }
+}
