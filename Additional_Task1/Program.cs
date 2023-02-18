741f9231deff3de66e625b5cad31d19ee1e6e9ae
Romanov Sergey <romanovcopy@gmail.com>
2023-02-18 17:37:35 +0300

    additional task1 end

--- Additional_Task1/Program.cs
+++ Additional_Task1/Program.cs
@@ -1,2 +1,48 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int[,] array = CreateArray( 5, 5 );
+PrintArray( array );
+array = Tran( array );
+Console.WriteLine( "Транспорированный относительно горизонтали массив :" );
+PrintArray( array );
+Console.ReadKey();
+
+//создание двумерного массива
+int[,] CreateArray( int lines, int columns )
+{
+    int[,] array = new int[lines, columns];
+    for (int i = 0; i < array.GetLength( 0 ); i++)
+    {
+        for (int j = 0; j < array.GetLength( 1 ); j++)
+        {
+            array[i, j] = new Random().Next( 0, 100 );
+        }
+    }
+    return array;
+}
+//вывод двумерного массива
+void PrintArray( int[,] array )
+{
+    for (int i = 0; i < array.GetLength( 0 ); i++)
+    {
+        for (int j = 0; j < array.GetLength( 1 ); j++)
+        {
+            Console.Write( $"{array[i, j].ToString( "##.##" )}\t" );
+        }
+        Console.WriteLine();
+    }
+}
+//транспорирование двумерного массива
+int[,] Tran( int[,] array )
+{
+    int tmp;
+    int last = array.GetLength(0)-1;
+    for (int i = 0; i < array.GetLength(0)/2; i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            tmp = array[i, j];
+            array[i, j] = array[last-i, j];
+            array[last - i, j] = tmp;
+        }
+    }
+    return array;
+}
