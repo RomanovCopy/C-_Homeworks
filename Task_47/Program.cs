615b692a9235bec6b9e04dcbb7e57cc157bfb844
Romanov Sergey <romanovcopy@gmail.com>
2023-02-18 15:12:23 +0300

    task 47 end

--- Task_47/Program.cs
+++ Task_47/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.*/
+Console.Write( "Размерность двумерного массива : " );
+int[] dim = Console.ReadLine().Split().Select( ( x ) => int.Parse( x ) ).ToArray();
+double[,] array = new double[dim[0], dim[1]];
+for(int i = 0; i < array.GetLength( 0 ); i++)
+{
+    for(int j = 0; j < array.GetLength( 1 ); j++)
+    {
+        array[i, j] = new Random().NextDouble() * 100;
+    }
+}
+for (int i = 0; i < array.GetLength( 0 ); i++)
+{
+    for (int j = 0; j < array.GetLength( 1 ); j++)
+    {
+        Console.Write( $"{ array[i, j].ToString("##.##")}\t");
+    }
+    Console.WriteLine();
+}
+Console.ReadKey();
+
+
+
